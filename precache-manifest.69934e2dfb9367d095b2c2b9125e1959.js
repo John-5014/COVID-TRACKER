5d4fe1ffa8929975d86d1cab579428d56b090860
john-5014 <kaiserjohn52@gmail.com>
2020-09-23 18:33:03 +0300

    Updates

---
+++ precache-manifest.69934e2dfb9367d095b2c2b9125e1959.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e844d59f9d682f99c07f798ab2ad94a5",
+    "url": "/COVID-TRACKER/index.html"
+  },
+  {
+    "revision": "7d5f16a99ef6417e0005",
+    "url": "/COVID-TRACKER/static/css/main.9bba03a1.chunk.css"
+  },
+  {
+    "revision": "fccb666c2bd0d71aa843",
+    "url": "/COVID-TRACKER/static/js/2.9d2af973.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-TRACKER/static/js/2.9d2af973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5f16a99ef6417e0005",
+    "url": "/COVID-TRACKER/static/js/main.55935a5d.chunk.js"
+  },
+  {
+    "revision": "72b1679dfac604867855",
+    "url": "/COVID-TRACKER/static/js/runtime-main.3f77430f.js"
+  },
+  {
+    "revision": "aa33c98b5a110acbf8faf27158cfb9d3",
+    "url": "/COVID-TRACKER/static/media/corona.aa33c98b.jpg"
+  }
+]);
